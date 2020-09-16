07d2c779abbdd02bc910b42d3791a112ae861e4c
Olysseus <olysseus@mail.ru>
2020-09-16 09:13:58 +0300

    start project

---
+++ src/scrap/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScrapConfig(AppConfig):
+    name = 'scrap'
+    verbose_name = 'Приложение по сбору вакансий'
---
+++ src/scrap/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+from scrap.utils import from_cyrillic_to_eng
+
+
+class City(models.Model):
+    name = models.CharField(max_length=80, verbose_name='Название города', unique=True)
+    slug = models.CharField(max_length=50, blank=True, verbose_name='Слаг', unique=True)
+
+    class Meta:
+        verbose_name = 'Название города'
+        verbose_name_plural = 'Названия городов'
+
+    def __str__(self):
+        return self.name
+
+    # autocomplete line 'slug'
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, verbose_name='Слаг', unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Название вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    id_city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    id_language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Язык')
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return '{0} , {1}'.format(self.title, self.company)
