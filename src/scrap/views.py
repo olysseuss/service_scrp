7013efc89b279505bba5f323015aab5c90b586d6
Olysseus <olysseus@mail.ru>
2020-09-19 20:51:41 +0300

    connected search form

--- src/scrap/views.py
+++ src/scrap/views.py
@@ -3,5 +3,15 @@ from .models import Vacancy
 
 
 def home(requests):
-    qs = Vacancy.objects.all()
+    print(requests.GET)
+    city = requests.GET.get('city')
+    language = requests.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['id_city__name'] = city
+        if language:
+            _filter['id_language__name'] = language
+        qs = Vacancy.objects.filter(**_filter)
     return render(requests, 'scrap/home.html', {'object_list': qs})
