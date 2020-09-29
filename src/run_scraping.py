b60ad668212b0eb625879a3d4ce509cba2cb8957
Olysseus <olysseus@mail.ru>
2020-09-30 01:14:13 +0300

    run_scraping was connected to django

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,4 +1,16 @@
+import os, sys
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+
+import django
+
+django.setup()
+
 from scrap.parsers import *
+from scrap.models import Vacancy, City, Language
+from django.db import DatabaseError
 
 parsers = (
     (work_ua, 'https://www.work.ua/ru/jobs-zaporizhzhya-python/'),
@@ -7,6 +19,9 @@ parsers = (
     (djinni_co, 'https://djinni.co/jobs/keyword-python/zaporizhzhya/'),
 )
 
+city = City.objects.filter(slug='zaporizhzhya').first()
+language = Language.objects.filter(slug='python').first()
+
 jobs, errors = [], []
 
 for func, url in parsers:
@@ -14,5 +29,12 @@ for func, url in parsers:
     jobs += j
     errors += e
 
-with open('work.txt', 'w', encoding='utf-8') as f_work:
-    f_work.write(str(jobs))
+for job in jobs:
+    vac = Vacancy(**job, id_city=city, id_language=language)
+    try:
+        vac.save()
+    except DatabaseError:
+        pass
+
+# with open('work.txt', 'w', encoding='utf-8') as f_work:
+#     f_work.write(str(jobs))
