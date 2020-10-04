e29f2cd79c373af9096caaaea26878a5ef4e3724
Olysseus <olysseus@mail.ru>
2020-10-05 02:06:04 +0300

    pagination, bootstrap pagination

--- src/scrap/views.py
+++ src/scrap/views.py
@@ -1,15 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .forms import FindForm
 from .models import Vacancy
 
 
-def home(requests):
-    print(requests.GET)
+def home_view(request):
+    #print(request.GET)
     form = FindForm()
-    city = requests.GET.get('city')
-    language = requests.GET.get('language')
-    qs = []
+    return render(request, 'scrap/home.html', {'form': form})
+
+def list_view(request):
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -17,4 +22,8 @@ def home(requests):
         if language:
             _filter['id_language__slug'] = language
         qs = Vacancy.objects.filter(**_filter)
-    return render(requests, 'scrap/home.html', {'object_list': qs, 'form': form})
+        paginator = Paginator(qs, 5) #show 10 cards per page
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
+    return render(request, 'scrap/list.html', context)
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from scrap.views import home
+from scrap.views import home_view, list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('', home_view, name='home'),
+    path('list', list_view, name='list'),
 ]
