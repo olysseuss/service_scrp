f0e1fc08a2b52059acfa15b00d4466815479f11f
Olysseus <olysseus@mail.ru>
2020-10-06 23:42:04 +0300

    User login form

---
+++ src/accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
+
+
+class UserLoginForm(forms.Form):
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInputattrs(attrs={'class': 'form-control'}))
+
+    def clean(self, *args, **kwargs):
+        email = self.cleaned_data.get('email').strip()
+        password = self.cleaned_data.get('password').strip()
+        if email and password:
+            qs = User.objects.filter(email='email')
+            if not qs.exists():
+                raise forms.ValidationError('Пользователя не существует')
+            if not check_password(password, qs[0].password):
+                raise forms.ValidationError('Пароль не верный')
+            user = authenticate(email=email, password=password)
+            if not user:
+                raise forms.ValidationError('Аккаунт отключен')
+        return super(UserLoginForm, self).clean(*args, **kwargs)
